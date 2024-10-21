a92d186a53e560426c2fc17655883f3efe5af79a
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-21 20:53:23 +0900

    multi cpu

--- main.py
+++ main.py
@@ -2,20 +2,35 @@ import json
 import urllib.request
 import os
 import datetime
+import psutil
 
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
+
+i = 0
+cpus = []
+for cpu in psutil.cpu_percent(interval=0.3, percpu=True):
+  cpus.append({
+    "name": f"CPU{i}",
+    "value": f"{cpu} %",
+    #"inline": True
+  })
+  i += 1
+
+vm = psutil.virtual_memory()
 
 msg = {
   "embeds": [{
     "fields": [
+      *cpus,
       {
-        "name": "CPU",
-        "value": "xx.x %"
-      },
+        "name": "Memory",
+        "value": f"{vm.percent}",
+      }
     ],
     "timestamp": datetime.datetime.now(datetime.UTC).isoformat(),
   }]
 }
+
 
 headers = {
   "Content-Type": "application/json",
