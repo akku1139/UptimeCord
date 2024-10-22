5d615bfb4a24c76a397983a6770f98dc51fab3f2
-akku- <151517253+akku1139@users.noreply.github.com>
2024-10-22 09:49:11 +0900

    再起動を省略

--- main.py
+++ main.py
@@ -3,46 +3,52 @@ import urllib.request
 import os
 import datetime
 import psutil
+import time
+import subprocess
 
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
-
-i = 0
-cpus = []
-for cpu in psutil.cpu_percent(interval=0.3, percpu=True):
-  cpus.append({
-    "name": f"CPU{i}",
-    "value": f"{cpu}%",
-    "inline": True,
-  })
-  i += 1
-
-vm = psutil.virtual_memory()
-
-msg = {
-  "embeds": [{
-    "fields": [
-      *cpus,
-      {
-        "name": "Memory",
-        "value": f"{vm.percent}%",
-      }
-    ],
-    "timestamp": datetime.datetime.now(datetime.UTC).isoformat(),
-  }]
-}
-
 
 headers = {
   "Content-Type": "application/json",
   "User-Agent": "DiscordBot (https://github.com/akku1139/UptimeCord, 0.0.1)",
 }
 
-req = urllib.request.Request(
-  WEBHOOK_URL,
-  json.dumps(msg).encode(),
-  headers,
-  method="POST",
-)
+while True:
+  i = 0
+  cpus = []
+  for cpu in psutil.cpu_percent(interval=0.3, percpu=True):
+    cpus.append({
+      "name": f"CPU{i}",
+      "value": f"{cpu}%",
+      "inline": True,
+    })
+    i += 1
 
-with urllib.request.urlopen(req) as res:
-  body = res.read()
+  vm = psutil.virtual_memory()
+
+  msg = {
+    "embeds": [{
+      "fields": [
+        *cpus,
+        {
+          "name": "Memory",
+          "value": f"{vm.percent}%",
+        }
+      ],
+      "timestamp": datetime.datetime.now(datetime.UTC).isoformat(),
+    }]
+  }
+
+  req = urllib.request.Request(
+    WEBHOOK_URL,
+    json.dumps(msg).encode(),
+    headers,
+    method="POST",
+  )
+
+  with urllib.request.urlopen(req) as res:
+    body = res.read()
+
+  time.sleep(60)
+  stdout = subprocess.run(["git","pull"], capture_output=True, text=True).stdout
+  log.info("git pull\n"+stdout)
